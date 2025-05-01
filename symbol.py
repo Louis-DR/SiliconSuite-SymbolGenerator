89677790dca65798201fbbbb77b612eaf37ed72a
Louis Duret-Robert <louisduret@gmail.com>
2025-05-01 15:30:18 +0200

    [Script] Drawing and font attributes + rename files

--- symbol.py
+++ symbol.py
@@ -1,4 +1,5 @@
 import argparse
+from .font_character_widths import font_character_widths
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Generate the SVG symbol of a hardware component from a description file.')
@@ -16,3 +17,43 @@ except FileNotFoundError:
 except Exception as exception:
   print(f"Error reading input file: {exception}.")
   exit(1)
+
+# Drawing parameters
+title_height           = 10 # Height of the title
+title_margin           =  0 # Bottom margin of the title
+subtitle_height        =  8# Height of the subtitle
+subtitle_margin        =  5 # Bottom margin of the subtitle
+ports_height           = 10 # Height of each port line
+ports_label_margin     = 10 # Margin between ports of either sides
+box_padding_top        =  2 # Box top padding
+box_padding_bottom     =  5 # Box bottom padding
+box_padding_sides      =  2 # Box bottom padding
+box_height_padding     = 10 # Box vertical padding
+port_arrow_length      = 20 # Length of the port arrows
+arrow_triangle_length  =  6 # Length of the port arrows
+arrow_triangle_height  =  4 # Length of the port arrows
+bus_line_distance      =  5 # Distance from end of line and the angled line
+bus_line_size          =  2 # Distance from line to each end of the angled line
+svg_padding            =  5 # Padding for the top, bottom, left, and right
+
+# Variables used in the SVG template
+template_variables = {}
+
+# Font attributes
+font_name = "helvetica"
+fonts = {
+  'title': {
+    'weight': "bold",
+    'size': 8
+  },
+  'subtitle': {
+    'weight': "normal",
+    'size': 6
+  },
+  'port': {
+    'weight': "normal",
+    'size': 6
+  }
+}
+template_variables['font_family'] = font_name
+template_variables['fonts']       = fonts
