fec2cd811b187c0e81a4c546aa801b4680980f5c
Louis Duret-Robert <louisduret@gmail.com>
2025-05-01 09:01:07 +0200

    [Script] Read input file

--- symbol.py
+++ symbol.py
@@ -4,3 +4,15 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate the SVG symbol of a hardware component from a description file.')
 parser.add_argument('input_file', help='Path to the symbol description file.')
 args = parser.parse_args()
+
+# Read input file
+input_description = None
+try:
+  with open(args.input_file, 'r') as input_file:
+    input_description = input_file.read()
+except FileNotFoundError:
+  print(f"Error: Input file not found at '{args.input_file}'.")
+  exit(1)
+except Exception as exception:
+  print(f"Error reading input file: {exception}.")
+  exit(1)
